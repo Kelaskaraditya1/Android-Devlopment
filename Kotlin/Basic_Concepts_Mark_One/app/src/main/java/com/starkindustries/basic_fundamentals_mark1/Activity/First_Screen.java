3b02371c7304136b37d1d29e51cd87da2af89d0f
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-06-10 11:07:45 +0530

    Project completed

---
+++ Kotlin/Basic_Concepts_Mark_One/app/src/main/java/com/starkindustries/basic_fundamentals_mark1/Activity/First_Screen.java
@@ -0,0 +1,26 @@
+package com.starkindustries.basic_fundamentals_mark1.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.starkindustries.basic_fundamentals_mark1.R;
+
+public class First_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_first_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
